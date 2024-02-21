1cbe8e7cc25a5dda74a7677287a72d422c4de8ee
Zsolt Palfi <zpalfi@cbr3s3.42barcelona.com>
2024-02-21 15:21:19 +0100

    Done 5, need to check ctrlc

---
+++ Python0/ex05/building.py
@@ -0,0 +1,52 @@
+"""_summary_
+"""
+
+import sys
+
+
+def count(text: str):
+    """_summary_
+    """
+    count_dict = {"total": 0, "upper": 0, "lower": 0, "punct": 0,
+                  "spaces": 0, "digits": 0}
+    count_dict["total"] += len(text)
+    for i in text:
+        if i.isupper():
+            count_dict["upper"] += 1
+        elif i.islower():
+            count_dict["lower"] += 1
+        elif i.isalnum():
+            count_dict["digits"] += 1
+        elif i.isspace():
+            count_dict["spaces"] += 1
+        else:
+            count_dict["punct"] += 1
+    print(f"The text contains {count_dict['total']} characters:\n"
+          f"{count_dict['upper']} upper letters\n"
+          f"{count_dict['lower']} lower letters\n"
+          f"{count_dict['punct']} punctuation marks\n"
+          f"{count_dict['spaces']} spaces\n"
+          f"{count_dict['digits']} digits")
+
+
+def main():
+    """_summary_
+    """
+    try:
+        if len(sys.argv) > 2:
+            raise AssertionError("more than one argument is provided")
+        if len(sys.argv) == 2:
+            count(sys.argv[1])
+        else:
+            try:
+                while True:
+                    text = input()
+                    count(text)
+            except EOFError:
+                sys.exit()
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
+
+if __name__ == "__main__":
+    main()
